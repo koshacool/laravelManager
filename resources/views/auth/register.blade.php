8eedd6868bf5566e8ef7e97121dc4bf700a427e1
koshacool <roman.kushytskyy@gmail.com>
2017-01-18 17:56:57 +0200

    add registration style from manager

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {{ csrf_field() }}
+    <div class="block">
+        <div class="authorizationForm">
+            <form action="{{ url('/register') }}" method="post" class="registration">
+                {{ csrf_field() }}
+                <div class="text">Registration</div>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
-                            {{--<label for="email" class="col-md-4 control-label">E-Mail Address</label>--}}
-
-                            {{--<div class="col-md-6">--}}
-                                {{--<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>--}}
-
-                                {{--@if ($errors->has('email'))--}}
-                                    {{--<span class="help-block">--}}
-                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
-                                    {{--</span>--}}
-                                {{--@endif--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div><span class='errorMessage' id="loginMessage"> {{ $errors->first('name') }} </span></div>
+                <div>
+                    <span class='login'>Login:</span>
+                    <input type="text" name='name' id="login" class="forValidation"
+                           placeholder="enter your login"
+                           value="{{ old('name') }}">
                 </div>
-            </div>
+
+                <div><span class='errorMessage' id="passwordMessage"> {{ $errors->first('password') }} </span></div>
+                <div>
+                    <span class='password'>Password:</span>
+                    <input type="password" name='password' id="password" class="forValidation"
+                           placeholder="enter your password">
+                </div>
+
+                <div><span class='errorMessage'
+                           id="repeatPasswordMessage"> {{ $errors->first('password_confirmation') }}</span></div>
+                <div>
+                    <span class='password'>Repeat password:</span>
+                    <input type="password" name='password_confirmation' class="forValidation"
+                           id="repeatPassword"
+                           placeholder="repeat password">
+                </div>
+
+                <div>
+                    <button class="buttonStyle" type="submit">
+                        <img src="/images/login.png">
+                        <span>Registraion</span>
+                    </button>
+                </div>
+
+            </form>
         </div>
     </div>
-</div>
 @endsection
